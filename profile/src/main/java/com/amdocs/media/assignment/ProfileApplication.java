344d46ff9e2da771957c8ddcd883e8089a3125b0
Tony Zhou <tz185027@ncr.com>
2022-05-23 14:44:04 -0400

    add <dependency>

--- profile/src/main/java/com/amdocs/media/assignment/ProfileApplication.java
+++ profile/src/main/java/com/amdocs/media/assignment/ProfileApplication.java
@@ -2,11 +2,13 @@ package com.amdocs.media.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableFeignClients
 @EnableTransactionManagement
 @EnableCaching
